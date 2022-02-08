896f4e40b900c4dff9f17e39a7c97702debe23a4
sundaramawasthi <sawasthi0001@gmail.com>
2022-02-08 21:03:58 +0530

    function

---
+++ python/Python basic/PythonFunction.py
@@ -0,0 +1,10 @@
+# function
+
+# define the function
+def fun1():  # function are define using def keyword then func name then colon that indicate start of function code block.
+    print("i am a function")
+
+
+fun1()  # function call by directly
+print(fun1())  # function print using print
+print(fun1)
