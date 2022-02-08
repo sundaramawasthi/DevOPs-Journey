704c1cc73823e17643be4cbe500b05b0b250e127
sundaramawasthi <sawasthi0001@gmail.com>
2022-02-08 20:39:23 +0530

    declare string and int and call function

--- python/Python basic/variable&expression.py
+++ python/Python basic/variable&expression.py
@@ -2,11 +2,26 @@
 
 # Declare the variable
 f = 0
-print(f)
+# print(f)
 
 # redeclare the variable
-f = "abc"
-print(f)
+#f = "abc"
+# print(f)
 
 # error : variable of different type can't be combine
-print("this is a string" + 123)
+# print("this is a string" + 123)  # show the type error
+
+
+# Convert to string
+#print("convert to string using str" + str(123))
+
+
+# global and local variable in function
+
+def somefunction():  # declare function def whose name is somefunction
+    f = "def"  # declare variable f =def in function
+    print(f)     # print the f value
+
+
+somefunction()   # call the somefunction
+print(f)         # print the f value
