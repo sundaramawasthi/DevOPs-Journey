8a57565c47c3f1faec6db79c79481d2735e97e3e
sundaramawasthi <sawasthi0001@gmail.com>
2022-02-08 22:19:04 +0530

    argument

--- python/Python basic/PythonFunction.py
+++ python/Python basic/PythonFunction.py
@@ -27,6 +27,13 @@ def power(num, x=1):
 # function with variable number of argument
 
 
+def multi_add(*arg):  # star char i can pass a variable number of argument
+    result = 0
+    for x in arg:
+        result = result + x
+    return result
+
+
 fun1()  # function call by directly
 print(fun1())  # function print using print
 print(fun1)  # function not call
@@ -37,3 +44,4 @@ print(fun1)  # function not call
 # print(cube(3))
 print(power(2))
 print(power(2, 3))
+print(multi_add(2, 3, 4, 5,))
