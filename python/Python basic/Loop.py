a657b470f0b78509a7cdee366cecbacf37a62ff8
sundaramawasthi <sawasthi0001@gmail.com>
2022-02-09 11:08:05 +0530

    python loop

--- python/Python basic/Loop.py
+++ python/Python basic/Loop.py
@@ -6,7 +6,18 @@ def main():
     while(x < 5):
         print(x)
         x += 1
+
+
 # For loop
+for x in range(5, 10):
+    print(x)
+    x += 1
+
+
+# uses a for loop over  a collection
+day = ['mon', 'twe', 'wed']
+for d in day:
+    print(d)
 
 
 if __name__ == "__main__":
