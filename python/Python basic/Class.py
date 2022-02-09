7caa3682d40039ae1e696a334e4ac386779e92cd
sundaramawasthi <sawasthi0001@gmail.com>
2022-02-09 11:44:52 +0530

    inheritance in class

--- python/Python basic/Class.py
+++ python/Python basic/Class.py
@@ -7,10 +7,23 @@ class myclass():
         print("method 2" + String)
 
 
+class anotherclass(myclass):
+    def method1(self):
+        myclass. method1(self)
+        print("anotherclass method 1")
+
+    def method2(self, someString):
+        print("another method 2")
+
+
 def main():
     c = myclass()
     c.method1()
     c.method2("give a string vallue ")
+
+    c2 = anotherclass()
+    c2.method1()
+    c2.method2("String")
 
 
 if __name__ == "__main__":
