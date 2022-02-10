6c3b000c66c850e5b446b8d4f7e6aeecf12cf0d5
sundaramawasthi <sawasthi0001@gmail.com>
2022-02-10 18:41:46 +0530

    update

---
+++ python/Python basic/workimgwithdateandTime.py
@@ -0,0 +1,12 @@
+# Date time in python
+
+from datetime import date
+from datetime import time
+from datetime import datetime
+
+
+def main():
+    # Get today date from the  class datetime
+    today = date.today()
+    print("today date is", today)
+    print(2+7)
